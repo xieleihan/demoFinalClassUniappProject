e4bd6c1490ac93df0394897f66413a916f2a97d5
xieleihan <xieleihan@gmail.com>
2024-06-18 16:56:48 +0800

    feat:create new content

---
+++ src/main/java/com/example/demofinalclassvueproject/DemoFinalClassVueProjectApplication.java
@@ -0,0 +1,22 @@
+package com.example.demofinalclassvueproject;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
+
+@SpringBootApplication
+public class DemoFinalClassVueProjectApplication {
+    private static final Logger LOG = LoggerFactory.getLogger(DemoFinalClassVueProjectApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(DemoFinalClassVueProjectApplication.class);
+
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("启动成功！！");
+        LOG.info("地址：\thttp://127.0.0.1:{}",env.getProperty("server.port"));
+    }
+
+}
---
+++ src/test/java/com/example/demofinalclassvueproject/DemoFinalClassVueProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demofinalclassvueproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoFinalClassVueProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
